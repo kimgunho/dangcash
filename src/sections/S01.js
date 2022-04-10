4bb7fb4b9df80af4787f2bc06e28ff980edc6bb2
Vi <yuyukgh@deeplehr.com>
2022-04-10 18:31:17 +0900

    Design : responsive title

--- src/sections/S01.js
+++ src/sections/S01.js
@@ -1,6 +1,9 @@
 import classNames from "classnames/bind";
 import styles from "./S01.module.scss";
 
+import deskTop_main from "../assets/images/s01/main.png";
+import mobile_main from "../assets/images/s01/main_mobile.png";
+
 const cx = classNames.bind(styles);
 function S01() {
   return (
@@ -10,7 +13,8 @@ function S01() {
           <span>반려견과 산책하면 포인트가 쌓인다</span>
           DANG CASH
         </h2>
-        <div className={cx("image")} />
+        <img className={cx("desktop")} src={deskTop_main} alt="" />
+        <img className={cx("mobile")} src={mobile_main} alt="" />
         <a href="" target={"_blank"}>
           카톡 산책하기
         </a>
